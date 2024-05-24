5a920e17f4452d291b701d542e5734b7fe595e82
Cadu Higueras <cadu.higueras@gmail.com>
2024-05-24 12:32:14 +0200

    Adding custom colors to catpuccin, custom command to show color palette and some other theme modifications

--- init.lua
+++ init.lua
@@ -13,3 +13,13 @@ vim.opt.rtp:prepend(lazypath)
 
 require("vim-options")
 require("lazy").setup("plugins")
+
+vim.api.nvim_create_user_command("ShowMochaPalette", function()
+	local mocha = require("catppuccin.palettes").get_palette("mocha")
+	vim.cmd("new") -- Open a new split
+	vim.cmd("setlocal buftype=nofile") -- Set buffer type to nofile
+	vim.cmd("setlocal bufhidden=wipe") -- Automatically wipe buffer when abandoned
+	vim.cmd("setlocal noswapfile") -- No swap file for this buffer
+	local output = vim.inspect(mocha)
+	vim.api.nvim_buf_set_lines(0, 0, -1, false, vim.split(output, "\n"))
+end, {})
--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,11 +1,34 @@
+vim.cmd.hi("Comment gui=none")
 return {
 	{
 		"catppuccin/nvim",
 		name = "catppuccin",
 		priority = 1000,
 		config = function()
-			--require("catppuccin").setup() - If config property is set, .setup() is called automatically
-			-- vim.cmd.colorscheme "catppuccin"
+			require("catppuccin").setup({
+				color_overrides = {
+					mocha = {
+						-- lavender = "#f6f931"
+						-- blue = "#eeff00",
+					},
+				},
+			})
+
+			-- require("catppuccin").setup() - If config property is set, .setup() is called automatically
+			vim.cmd.colorscheme("catppuccin-mocha")
+			local function_and_method_color = "#ffcf7a"
+			local custom_property_color = "#bac2de"
+			-- For functions and Method
+			vim.api.nvim_set_hl(0, "Function", { fg = function_and_method_color })
+			vim.api.nvim_set_hl(0, "Method", { fg = function_and_method_color })
+			-- HTML and JSX properties
+			vim.api.nvim_set_hl(0, "htmlArg", { fg = custom_property_color })
+			vim.api.nvim_set_hl(0, "htmlTagN", { fg = custom_property_color })
+			vim.api.nvim_set_hl(0, "jsxTagName", { fg = custom_property_color })
+
+			-- For Treesitter
+			vim.api.nvim_set_hl(0, "@property", { fg = custom_property_color })
+			vim.api.nvim_set_hl(0, "@tag.attribute", { fg = custom_property_color })
 		end,
 	},
 	{
@@ -25,10 +48,36 @@ return {
 			-- Load the colorscheme here.
 			-- Like many other themes, this one has different styles, and you could load
 			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-			vim.cmd.colorscheme("tokyonight-night")
+			-- vim.cmd.colorscheme("tokyonight-storm")
 
 			-- You can configure highlights by doing something like:
-			vim.cmd.hi("Comment gui=none")
+			-- vim.cmd.hi("Comment gui=none")
+		end,
+	},
+	{
+		"doums/darcula",
+		priority = 1000,
+		init = function()
+			-- vim.cmd.colorscheme("darcula")
+
+			-- You can configure highlights by doing something like:
+			-- vim.cmd.hi("Comment gui=none")
+		end,
+	},
+	{
+		"ellisonleao/gruvbox.nvim",
+		priority = 1000,
+		config = function()
+			-- vim.cmd.hi("Comment gui=none")
+			-- vim.cmd.colorscheme("gruvbox")
+		end,
+	},
+	{
+		"rebelot/kanagawa.nvim",
+		priority = 1000,
+		config = function()
+			-- vim.cmd.hi("Comment gui=none")
+			-- vim.cmd.colorscheme("gruvbox")
 		end,
 	},
 }
